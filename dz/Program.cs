a39f76e572e288bfc9e6b00250a9551ab3a7cbd9
Sergei <sandeeey@example.com>
2023-03-23 20:56:38 +0300

    dz_array

--- dz/Program.cs
+++ dz/Program.cs
@@ -6,29 +6,9 @@ string[] DelPunctuatMark(string str)
     string[] delMark = { ".", ",", "(", ")" };
     for (int i = 0; i < delMark.Length; i++)
     {
-         str = str.Replace(delMark[i], string.Empty);
+        str = str.Replace(delMark[i], string.Empty);
     }
-    return str.Split(' ');
+    return str.Split(" ");
 }
 
 
-string[] ArreyWords(string[]str)
-{
-    int maxLength=3;
-    string newStr = string.Empty;
-    for (int i = 0; i < str.Length; i++)
-    {
-        if (str[i].Length <= maxLength)
-        {
-            newStr=newStr=str[i]+" ;";
-        }
-    }
-    newStr=newStr.Trim();
-    return newStr.Split(" ");
-}
-
-Console.WriteLine($"Текст который вы ввели: \n");
-Console.WriteLine($"{text} \n");
-Console.WriteLine("Слова не превышающие длину 3 символа в данном тексте");
-string[] array=ArreyWords(DelPunctuatMark(text));
-Console.Write($"\n {string.Join(' ', array)}");
