a92c088e7f3d2f66feaf314e1e53567bd504d93d
Sergei <sandeeey@example.com>
2023-03-23 20:50:23 +0300

    dz_print

--- dz/Program.cs
+++ dz/Program.cs
@@ -26,3 +26,9 @@ string[] ArreyWords(string[]str)
     newStr=newStr.Trim();
     return newStr.Split(" ");
 }
+
+Console.WriteLine($"Текст который вы ввели: \n");
+Console.WriteLine($"{text} \n");
+Console.WriteLine("Слова не превышающие длину 3 символа в данном тексте");
+string[] array=ArreyWords(DelPunctuatMark(text));
+Console.Write($"\n {string.Join(' ', array)}");
